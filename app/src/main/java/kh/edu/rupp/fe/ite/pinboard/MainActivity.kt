fa968509556498824e717ddc82a63b2058ba3830
Alujack <yeantouch12345@gmail.com>
2025-09-27 17:41:41 +0700

    init the jetpack compose app

---
+++ app/src/main/java/kh/edu/rupp/fe/ite/pinboard/MainActivity.kt
@@ -0,0 +1,47 @@
+package kh.edu.rupp.fe.ite.pinboard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kh.edu.rupp.fe.ite.pinboard.ui.theme.PinBoardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PinBoardTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Team at ITE",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PinBoardTheme {
+        Greeting("Team at ITE")
+    }
+}
